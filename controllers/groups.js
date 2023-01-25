11638176ca96084bb79ce1bbd3baa887e79cb3f1
uedi <uedi@users.noreply.github.com>
2023-01-25 18:44:27 +0200

    create group

--- controllers/groups.js
+++ controllers/groups.js
@@ -1,6 +1,7 @@
 const groupRouter = require('express').Router()
 const auth = require('../utils/auth')
 const { User, Group } = require('../models')
+const { userExtractor } = require('../utils/middleware')
 
 groupRouter.get('/', auth, async (req, res) => {
     const user = await User.findByPk(req.user.id)
@@ -12,6 +13,21 @@ groupRouter.get('/', auth, async (req, res) => {
     const groups = await Group.findAll({ owner: user.id })
     
     return res.status(200).json(groups)
+})
+
+groupRouter.post('/', auth, userExtractor, async (req, res) => {
+    const body = req.body
+
+    if(!body?.name) {
+        return res.status(400).end()
+    }
+
+    const newGroup = await Group.create({
+        name: body.name,
+        owner: req.savedUser.id
+    })
+
+    return res.status(201).json(newGroup)
 })
 
 module.exports = groupRouter
