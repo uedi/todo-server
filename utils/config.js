726a406268041ba45a1b1599ad3b37ddc54d5404
uedi <uedi@users.noreply.github.com>
2023-01-25 19:40:33 +0200

    add ssl setting to exports

--- utils/config.js
+++ utils/config.js
@@ -5,5 +5,5 @@ const DATABASE_URL = process.env.DATABASE_URL
 const DATABASE_SSL = process.env.NO_DATABASE_SSL ? false : true
 
 module.exports = {
-    PORT, DATABASE_URL
+    PORT, DATABASE_URL, DATABASE_SSL
 }
