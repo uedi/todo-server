97b3b3b33bd14191d721648d3d96abb90173b2dd
uedi <uedi@users.noreply.github.com>
2023-02-02 10:49:29 +0200

    install express-async-errors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const middleware = require('./utils/middleware')
 const signupRouter = require('./controllers/signup')
