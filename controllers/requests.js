d58b2ebefd18f4ab63f47121a5545a3cdc84210a
uedi <uedi@users.noreply.github.com>
2023-02-13 11:17:58 +0200

    membership responses

--- controllers/requests.js
+++ controllers/requests.js
@@ -22,4 +22,47 @@ requestsRouter.get('/', auth, userExtractor, async (req, res) => {
     return res.status(200).json(dataToSend)
 })
 
+requestsRouter.post('/membership', auth, userExtractor, async (req, res) => {
+    const body = req.body
+
+    if(!body?.groupId) {
+        return res.status(400).end()
+    }
+
+    const membership = await Membership.findOne({
+        where: {
+            groupId: body.groupId,
+            userId: req.savedUser.id
+        }
+    })
+
+    if(!membership) {
+        return res.status(404).end()
+    }
+
+    if(!membership.pending) {
+        console.log('membership is not pending')
+    }
+
+    if(body.reject) {
+        await membership.destroy()
+    } else {
+        membership.pending = false
+        await membership.save()
+    }
+
+    const memberships = await Membership.findAll({
+        where: {
+            userId: req.savedUser.id,
+            pending: true
+        },
+        include: {
+            model: Group,
+            attributes: ['name', 'id']
+        }
+    })
+
+    return res.status(200).json(memberships)
+})
+
 module.exports = requestsRouter
