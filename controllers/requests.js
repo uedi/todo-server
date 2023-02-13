30a8a49b687839537659cf1013a87587a678be69
uedi <uedi@users.noreply.github.com>
2023-02-13 17:15:49 +0200

    accept membership returns group

--- controllers/requests.js
+++ controllers/requests.js
@@ -62,7 +62,14 @@ requestsRouter.post('/membership', auth, userExtractor, async (req, res) => {
         }
     })
 
-    return res.status(200).json(memberships)
+    const group = await Group.findByPk(body.groupId, {
+        include: {
+            model: User, as: 'users',
+            attributes: ['name', 'username', 'id']
+        }
+    })
+
+    return res.status(200).json({ memberships: memberships, group: group })
 })
 
 module.exports = requestsRouter
