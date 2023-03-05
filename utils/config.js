656ab82ef1d43a768fb4f81159e0a5287af1471c
uedi <uedi@users.noreply.github.com>
2023-03-05 16:24:30 +0200

    fly io production configuration

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const DATABASE_URL = process.env.NODE_ENV === 'test'
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL
